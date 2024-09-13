ce00ecebc955b3d0158bf5e8276da49e84d9ff7f
Eric Weber <eric@weber.lol>
2024-09-12 19:57:23 -0700

    initial commit

---
+++ src/jack_nam.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char** argv) {
+  std::cout << std::endl << "Test yolo" << std::endl << std::endl;
+  return 0;
+}
+
+// #include 
+
+// jack_client_open (client_name, JackNoStartServer, &status));
